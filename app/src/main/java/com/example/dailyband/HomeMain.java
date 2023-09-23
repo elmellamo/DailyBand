ce39e051939c9738c357df8f0e7b0373170ebdda
elmellamo <th6986@naver.com>
2023-09-23 23:59:10 +0900

    홈화면 대강 구현

--- app/src/main/java/com/example/dailyband/HomeMain.java
+++ app/src/main/java/com/example/dailyband/HomeMain.java
@@ -1,11 +1,13 @@
 package com.example.dailyband;
 
-        import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
 
-        import androidx.appcompat.app.AppCompatActivity;
-public class HomeMain extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState){
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeMain extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homemain);
     }
