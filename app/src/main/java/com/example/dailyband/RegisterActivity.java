b90a53c3e002c0d15b17afd63b8fb6bce84c50d6
mywoospam <dobbydavid1@naver.com>
2023-10-02 18:04:08 +0900

    회원가입 쪽 레이아웃 전체적으로 했습니다.

--- app/src/main/java/com/example/dailyband/RegisterActivity.java
+++ app/src/main/java/com/example/dailyband/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,8 +23,8 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth; // 파이어 베이스 인증
     private DatabaseReference mDatabaseRef; // 실시간 데이터 베이스
     private EditText nameEdittext,emailEdittext, pwEdittext,pwchkEdittext;
-    private Button gotologinBtn, registerBtn;
-
+    private Button  registerBtn;
+    private TextView gotologinBtn;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
