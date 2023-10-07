3621411153aa6a8a07d3c10ea7879e130eb218aa
mywoospam <dobbydavid1@naver.com>
2023-10-07 14:14:23 +0900

    로컬 사진 접근

--- app/src/main/java/com/example/dailyband/SettingActivity.java
+++ app/src/main/java/com/example/dailyband/SettingActivity.java
@@ -1,16 +1,75 @@
 package com.example.dailyband;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+
 public class SettingActivity extends AppCompatActivity {
+    private ImageView profileImg;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        profileImg = findViewById(R.id.bird_img);
+
+        profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                pickImageActivityResultLauncher.launch(intent);
+            }
+        });
     }
 
+    public String getPathFromURI(Uri contentUri){
+        String res = null;
+
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null,null);
+        if(cursor.moveToFirst()){
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+
+        return res;
+    }
+
+    ActivityResultLauncher<Intent> pickImageActivityResultLauncher=
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if(result.getResultCode()== Activity.RESULT_OK){
+                                Intent data = result.getData();
+                                Uri selectedImageUri = data.getData();
+
+                                final String path = getPathFromURI(selectedImageUri);
+                                if(path!=null){
+                                    File f = new File(path);
+                                    selectedImageUri = Uri.fromFile(f);
+                                }
+                                profileImg.setImageURI(selectedImageUri);
+                            }
+                        }
+                    });
 }
