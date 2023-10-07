e0e63c3f987f497d23e92aeb34b6bdcf31e3f796
mywoospam <dobbydavid1@naver.com>
2023-10-07 16:39:37 +0900

    로컬 사진 변경 가져오기 가능

--- app/src/main/java/com/example/dailyband/SettingActivity.java
+++ app/src/main/java/com/example/dailyband/SettingActivity.java
@@ -1,75 +1,135 @@
 package com.example.dailyband;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.io.File;
+import java.util.List;
 
 public class SettingActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int REQUEST_IMAGE_PICK = 1;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
     private ImageView profileImg;
+    private Uri selectedImageUri = null;
+    private Uri userImage=null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
         profileImg = findViewById(R.id.bird_img);
+
+
+        String userUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference storageRef = storage.getReference().child("profile_images");
+        StorageReference imageRef = storageRef.child(userUID+".jpg");
+
+        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+            String imageUrl = uri.toString();
+            Glide.with(this)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.brid_second_img)
+                    .error(R.drawable.brid_second_img)
+                    .into(profileImg); // profileImage는 앱의 이미지뷰 객체
+        });
 
         profileImg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                pickImageActivityResultLauncher.launch(intent);
+            public void onClick(View v) {
+                // Start photo picker to select an image
+                openGallery();
             }
         });
     }
-
-    public String getPathFromURI(Uri contentUri){
-        String res = null;
-
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(contentUri, proj, null, null,null);
-        if(cursor.moveToFirst()){
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            res = cursor.getString(column_index);
-        }
-        cursor.close();
-
-        return res;
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
     }
 
-    ActivityResultLauncher<Intent> pickImageActivityResultLauncher=
-            registerForActivityResult(
-                    new ActivityResultContracts.StartActivityForResult(),
-                    new ActivityResultCallback<ActivityResult>() {
-                        @Override
-                        public void onActivityResult(ActivityResult result) {
-                            if(result.getResultCode()== Activity.RESULT_OK){
-                                Intent data = result.getData();
-                                Uri selectedImageUri = data.getData();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-                                final String path = getPathFromURI(selectedImageUri);
-                                if(path!=null){
-                                    File f = new File(path);
-                                    selectedImageUri = Uri.fromFile(f);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            selectedImageUri = data.getData();
+
+            // 이미지 뷰에 선택한 이미지 표시
+            profileImg.setImageURI(selectedImageUri);
+
+            // Firebase Storage에 이미지 업로드
+            uploadImageToFirebase(selectedImageUri);
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        if (imageUri != null) {
+            StorageReference storageRef = storage.getReference().child("profile_images");
+            StorageReference imageRef = storageRef.child(userId+".jpg"); // 저장될 파일 이름
+
+            UploadTask uploadTask = imageRef.putFile(imageUri);
+
+            uploadTask.addOnSuccessListener(taskSnapshot -> {
+                // 업로드 성공 시 이미지 URL을 얻어올 수 있음
+                imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String imageUrl = uri.toString();
+                    Toast.makeText(SettingActivity.this, "이미지 수정 성공", Toast.LENGTH_SHORT).show();
+
+                    // TODO: imageUrl을 사용하여 데이터베이스에 저장하거나 다른 작업 수행
+
+                    DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+                    databaseRef.child("user_photo").child(userId).child("profileImageUrl").setValue(imageUrl)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SettingActivity.this, "이미지 수정 완료", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(SettingActivity.this, "이미지 수정 실패", Toast.LENGTH_SHORT).show();
                                 }
-                                profileImg.setImageURI(selectedImageUri);
-                            }
-                        }
-                    });
+                            });
+                });
+            }).addOnFailureListener(e -> {
+                // 업로드 실패 시 에러 처리
+                Toast.makeText(SettingActivity.this, "이미지 수정 실패", Toast.LENGTH_SHORT).show();
+            });
+        }
+    }
+
 }
