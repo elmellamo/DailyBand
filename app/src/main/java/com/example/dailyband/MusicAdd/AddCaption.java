c93d833da55be5946140b9af6892f7d393d7b7da
elmellamo <th6986@naver.com>
2023-10-11 00:38:53 +0900

    녹음 버튼?

---
+++ app/src/main/java/com/example/dailyband/MusicAdd/AddCaption.java
@@ -0,0 +1,14 @@
+package com.example.dailyband.MusicAdd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dailyband.R;
+
+public class AddCaption extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_caption);
+    }
+}
