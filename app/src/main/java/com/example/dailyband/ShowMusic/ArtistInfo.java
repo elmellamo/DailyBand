df5e256ebf1f40fe651572044d85ee6c5c37dce7
mywoospam <dobbydavid1@naver.com>
2023-11-08 02:52:24 +0900

    스크롤 에러 해결

--- app/src/main/java/com/example/dailyband/ShowMusic/ArtistInfo.java
+++ app/src/main/java/com/example/dailyband/ShowMusic/ArtistInfo.java
@@ -3,11 +3,20 @@ package com.example.dailyband.ShowMusic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dailyband.Collection.CollectionActivity;
+import com.example.dailyband.Home.HomeMain;
+import com.example.dailyband.Love.LoveActivity;
+import com.example.dailyband.MusicAdd.AddMusic;
 import com.example.dailyband.R;
+import com.example.dailyband.Setting.NewSettingActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,6 +29,8 @@ public class ArtistInfo extends AppCompatActivity {
     private TextView artist_detail_textview,info_text;
     private String artist, title, postId, userUid;
     private boolean isLiked;
+    private ImageButton addbtn, setbtn, librarybtn, myInfobtn, homeBtn;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +38,8 @@ public class ArtistInfo extends AppCompatActivity {
 
         info_text = findViewById(R.id.info_text);
         artist_detail_textview = findViewById(R.id.artist_detail_textview);
+        artist_detail_textview.setMovementMethod(new ScrollingMovementMethod());
+
         Intent intent = getIntent();
         artist = intent.getStringExtra("artist_intent");
         title = intent.getStringExtra("title_intent");
@@ -34,6 +47,11 @@ public class ArtistInfo extends AppCompatActivity {
         userUid = intent.getStringExtra("userUid_intent");
         //isLiked = intent.getBooleanExtra("isLiked_intent");
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        homeBtn = findViewById(R.id.homeBtn);
+        myInfobtn = findViewById(R.id.myInfobtn);
+        librarybtn = findViewById(R.id.librarybtn);
+        setbtn = findViewById(R.id.setbtn);
+        addbtn = findViewById(R.id.addbtn);
 
         info_text.setText(artist);
         mDatabase.child("user_introduce").addValueEventListener(new ValueEventListener() {
@@ -52,5 +70,40 @@ public class ArtistInfo extends AppCompatActivity {
 
             }
         });
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myStartActivity(HomeMain.class);
+            }
+        });
+        myInfobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myStartActivity(LoveActivity.class);
+            }
+        });
+        librarybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myStartActivity(CollectionActivity.class);
+            }
+        });
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ArtistInfo.this, AddMusic.class);
+                intent.putExtra("parent_Id", "ori");
+                startActivity(intent);
+            }
+        });
+        setbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { myStartActivity(NewSettingActivity.class);    }
+        });
+    }
+    private void myStartActivity(Class c){
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
